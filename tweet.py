2cf452c21d70ffde08e897f0bfeddd0fa7b6e7f7
ch1t7n <chetan1pandita@gmail.com>
2021-10-14 17:41:09 +0530

    python code to get the tweets

--- tweet.py
+++ tweet.py
@@ -1,10 +1,22 @@
 import tweepy
-
+consumer_key= ""
+consumer_secret=""
+access_token = ""
+access_token_secret = ""
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
-
-api = tweepy.API(auth)
-
-public_tweets = api.home_timeline()
-for tweet in public_tweets:
-    print(tweet.text)
+while True:
+    enter_user = input("Enter user:")
+    userID = enter_user
+    api = tweepy.API(auth)
+    tweets = api.user_timeline(screen_name=userID, count=140, include_rts = False, tweet_mode = 'extended'
+                           )
+    for info in tweets[:]:
+        print(info.created_at)
+        print(info.full_text)
+        print("\n")
+    x = input("Want to continue(y/n): ")
+    if x == "y":
+        continue
+    else:
+        break
